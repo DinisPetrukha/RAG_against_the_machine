54c5b37e470faaa7875780f631b8c1a1162fd0b2
Dinis Petrukha <dpetrukh@Diniss-MacBook-Air.local>
2026-09-20 15:24:41 +0100

    add dataset for testing in the future

--- src/__main__.py
+++ src/__main__.py
@@ -1,1 +1,24 @@
+import fire
+
+
+def index() -> None:
+    print("index")
+
+def search() -> None:
+    print("search")
+
+def search_dataset() -> None:
+    print("search_dataset")
+
+def answer() -> None:
+    print("answer")
+
+def answer_dataset() -> None:
+    print("answer_dataset")
+
+def evaluate() -> None:
+    print("evaluate")
+
+
+
 print("Hello, World!")
